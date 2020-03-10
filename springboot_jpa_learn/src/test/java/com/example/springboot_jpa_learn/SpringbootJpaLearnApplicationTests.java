01ae8f31d85ac60660ef4d04406b79df2bb17884
鹏 刘 <152628liupeng@gmail.com>
2020-03-10 21:54:41 +0800

    global primary initialization

---
+++ springboot_jpa_learn/src/test/java/com/example/springboot_jpa_learn/SpringbootJpaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jpa_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
